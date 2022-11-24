a3343bb16d7dc69abd27b11c49bcc08b9addfae4
willianmenezess <willmenezes@outlook.com.br>
2022-11-24 19:38:22 -0300

    finalizando exercicio 3 - if/else

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/exercicios-do-dia/if-else-3.js
@@ -0,0 +1,2 @@
+// Utilize if/else para escrever um código que, dado um valor recebido como parâmetro, retorne “positive” se esse valor for positivo, “negative” se for negativo, e caso não seja nem positivo e nem negativo retorne “zero”.
+
