ab54fca7eef0923cb2f00df14a6536bcadd07aac
willianmenezess <willmenezes@outlook.com.br>
2022-11-25 21:17:57 -0300

    exercicio 1 e 2

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-array-e-for/exercicios-do-dia/exercicio-2.js
@@ -0,0 +1,10 @@
+// Some todos os valores contidos no array e imprima o resultado;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+soma = 0;
+
+for (var index = 0 ; index < numbers.length ; index += 1) {
+    soma = soma + numbers[index];
+}
+
+console.log(soma);
