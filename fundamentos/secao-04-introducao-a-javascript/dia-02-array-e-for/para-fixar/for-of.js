5079c6ef343986c5238acf6395ab3756bcdb1685
willianmenezess <willmenezes@outlook.com.br>
2022-11-25 21:00:49 -0300

    exemplo for-of para fixar

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-array-e-for/para-fixar/for-of.js
@@ -0,0 +1,9 @@
+// Utilize o for/of para imprimir os elementos da lista names com o console.log():
+
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let lista of names) {
+    console.log (lista);
+}
+
