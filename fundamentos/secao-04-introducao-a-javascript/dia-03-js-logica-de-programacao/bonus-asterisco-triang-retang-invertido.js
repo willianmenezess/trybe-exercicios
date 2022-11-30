425637c442ec75f78f94566a64005dd279435ec2
willianmenezess <willmenezes@outlook.com.br>
2022-11-30 13:04:48 -0300

    exercicio bonus triangulo retangulo invertido

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-js-logica-de-programacao/bonus-asterisco-triang-retang-invertido.js
@@ -0,0 +1,24 @@
+// Programa que imprima um triângulo retângulo com 5 asteriscos de base. 
+
+// Agora, inverta o lado do triângulo
+
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+let inputPosition = n - 1;
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  for (let columnIndex = 0; columnIndex < n; columnIndex += 1) {
+    if (columnIndex < inputPosition) {
+      inputLine = inputLine + ' ';
+    } else {
+      inputLine = inputLine + symbol;
+    }
+  }
+  console.log(inputLine);
+  inputLine = '';
+  inputPosition -= 1;
+};
+
+
+
--- fundamentos/secao-04-introducao-a-javascript/dia-03-js-logica-de-programacao/bonus-asterisco-triang-retangulo.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-js-logica-de-programacao/bonus-asterisco-triang-retangulo.js
@@ -9,7 +9,5 @@ for (let index = 1 ; index <= n ; index += 1){
     console.log(lineAsteris);
 }
 
-// for (let index2 = 1 ; index2 <= n ; index2 +=1){
-//     console.log(lineAsteris);
-// }
+
 
