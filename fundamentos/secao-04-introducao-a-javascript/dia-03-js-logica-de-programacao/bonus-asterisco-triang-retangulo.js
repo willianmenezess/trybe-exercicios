9d084fe8ac719efa1ab483e42a5ab38dae839761
willianmenezess <willmenezes@outlook.com.br>
2022-11-30 12:06:39 -0300

    exercicio bonus triangulo retangulo de asteriscos

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-js-logica-de-programacao/bonus-asterisco-triang-retangulo.js
@@ -0,0 +1,15 @@
+// Faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. 
+
+let n = 5;
+let symbol = '*';
+let lineAsteris = '';
+
+for (let index = 1 ; index <= n ; index += 1){
+    lineAsteris = lineAsteris + symbol;
+    console.log(lineAsteris);
+}
+
+// for (let index2 = 1 ; index2 <= n ; index2 +=1){
+//     console.log(lineAsteris);
+// }
+
