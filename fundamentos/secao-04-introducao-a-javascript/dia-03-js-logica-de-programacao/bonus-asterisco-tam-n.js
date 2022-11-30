ca021b8df6459683b2d8defbbb4eaebda4897d27
willianmenezess <willmenezes@outlook.com.br>
2022-11-30 01:20:17 -0300

    bonus quadrado de asteriscos

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-js-logica-de-programacao/bonus-asterisco-tam-n.js
@@ -0,0 +1,11 @@
+let n = 6;
+let symbol = '*';
+let lineAsteris = '';
+
+for (let index = 1 ; index <= n ; index += 1){
+    lineAsteris = lineAsteris + symbol;
+}
+
+for (let index2 = 1 ; index2 <= n ; index2 +=1){
+    console.log(lineAsteris);
+}
