ad6c12ccc046ae86081736eeb153d8be644fdfde
willianmenezess <willmenezes@outlook.com.br>
2022-12-01 19:57:32 -0300

    resolvendo exercicio function valor que mais se repete

--- fundamentos/secao-04-introducao-a-javascript/dia-04-js-objetos-e-funcoes/exercicios-funcoes/function-mais-se-repete.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-js-objetos-e-funcoes/exercicios-funcoes/function-mais-se-repete.js
@@ -1,30 +1,30 @@
 // Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
-
 // Array de teste: [2, 3, 2, 5, 8, 2, 3];.
-
 // Valor esperado no retorno da função: 2.
 
 function moreRepeated(array) {
 
     let moreRepetitions = 0;
+    let valueMoreRepetitions = 0;
 
-    for (index = 0; index < array.lenth ; index +=1) {
+    for (index = 0; index < array.length; index += 1) {
         let repetitions = 0;
-        
 
-        for (index2 = 0 ; index2 < array.length ; index2 += 1) {
-            if (array[index] === array[index2]){
+
+        for (index2 = 0; index2 < array.length; index2 += 1) {
+            if (array[index] === array[index2]) {
                 repetitions = repetitions + 1;
             }
         }
 
         if (repetitions > moreRepetitions) {
-            moreRepetitions = array[index];
+            moreRepetitions = repetitions;
+            valueMoreRepetitions = array[index];
         }
 
     }
 
-    return(moreRepetitions);
+    return (valueMoreRepetitions);
 }
 
 console.log(moreRepeated([2, 3, 2, 5, 8, 2, 3]));
