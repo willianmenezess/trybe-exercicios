b270c82fa76ab6139782213f1fc5afeadca98cdc
willianmenezess <willmenezes@outlook.com.br>
2022-12-01 14:00:07 -0300

    exercicio function numero que mais se repete num array

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-js-objetos-e-funcoes/exercicios-funcoes/function-mais-se-repete.js
@@ -0,0 +1,31 @@
+// Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+
+// Array de teste: [2, 3, 2, 5, 8, 2, 3];.
+
+// Valor esperado no retorno da função: 2.
+
+function moreRepeated(array) {
+
+    let moreRepetitions = 0;
+
+    for (index = 0; index < array.lenth ; index +=1) {
+        let repetitions = 0;
+        
+
+        for (index2 = 0 ; index2 < array.length ; index2 += 1) {
+            if (array[index] === array[index2]){
+                repetitions = repetitions + 1;
+            }
+        }
+
+        if (repetitions > moreRepetitions) {
+            moreRepetitions = array[index];
+        }
+
+    }
+
+    return(moreRepetitions);
+}
+
+console.log(moreRepeated([2, 3, 2, 5, 8, 2, 3]));
+
