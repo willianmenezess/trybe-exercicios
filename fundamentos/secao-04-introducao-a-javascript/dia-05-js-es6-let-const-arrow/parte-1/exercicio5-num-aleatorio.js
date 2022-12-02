6c7917e70abbc2e80d91fb02b27349520b329504
willianmenezess <willmenezes@outlook.com.br>
2022-12-01 21:57:59 -0300

    exercicio5-parte1 arrowfuncion1

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-05-js-es6-let-const-arrow/parte-1/exercicio5-num-aleatorio.js
@@ -0,0 +1,10 @@
+// Transforme a função numeroAleatorio em uma arrow function;
+
+// function numeroAleatorio() {
+//     return Math.random()
+//   }
+//   console.log(numeroAleatorio());
+
+
+  const numeroAleatorio = () => Math.random();
+  console.log(numeroAleatorio());
