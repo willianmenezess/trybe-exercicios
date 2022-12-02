94ed5de45124039c181a1d268bea9f90e4544aa8
willianmenezess <willmenezes@outlook.com.br>
2022-12-01 22:52:27 -0300

    exercicio8-parte1: if/else ternary operation

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-05-js-es6-let-const-arrow/parte-1/exercicio8-if-else-ternary-op.js
@@ -0,0 +1,17 @@
+// Altere a expressão if/else utilizando ternary operator.
+
+
+// let speed = 90;
+// const speedCar = (speed) => {
+//   if (speed >= 120) {
+//     return `Você ultrapassou o limite de velocidade`;
+//   } else {
+//     return `Você está na velocidade permitida`;
+//   }
+// };
+// console.log(speedCar(speed));
+
+let speed = 90;
+const speedCar = (speed) => (speed>=120) ? 'Você ultrapassou o limite de velocidade' : 'Você está na velocidade permitida';
+
+console.log(speedCar(speed));
