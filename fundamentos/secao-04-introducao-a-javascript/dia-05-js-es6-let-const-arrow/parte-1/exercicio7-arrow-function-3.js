e431beda292ecc2685f095a96e57c9d8163657ed
willianmenezess <willmenezes@outlook.com.br>
2022-12-01 22:45:04 -0300

    exercicios 5 e 6 - arrow function

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-05-js-es6-let-const-arrow/parte-1/exercicio7-arrow-function-3.js
@@ -0,0 +1,12 @@
+// Transforme a função nomeCompleto em uma arrow function;
+
+
+//  function nomeCompleto(nome, sobrenome) {
+//     return `${nome} ${sobrenome}`
+//   }
+//   let nome = 'Ivan';
+//   let sobrenome = 'Pires';
+//   console.log(nomeCompleto(nome, sobrenome));
+
+  const nomeCompleto = (nome, sobrenome) => `${nome} ${sobrenome}`;
+  console.log(nomeCompleto('Ivan', 'Pires'));
