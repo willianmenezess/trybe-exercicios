a93c2050cb3aee2e9817c650bed52c4436ed6a46
willianmenezess <willmenezes@outlook.com.br>
2022-12-07 22:02:11 -0300

    requisito 1 - funcao que muda o texto na tag p

--- fundamentos/secao-05-js-dom-eventos-web-storage/dia-01-js-dom-e-seletores/exercicio-mnipulacao-de-dom/script.js
+++
@@ -1,5 +0,0 @@
-// const header = document.getElementById('header-container');
-// header.style.backgroundColor = 'rgb(0, 176, 105)';
-
-// const emergencyTasks = document.getElementsByClassName('emergency-tasks')[0];
-// emergencyTasks.style.backgroundColor = 'rgb(248, 126, 96)';
