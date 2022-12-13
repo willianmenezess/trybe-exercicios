d8cc887446856e762228f81fbff2db8abd658d8a
willianmenezess <willmenezes@outlook.com.br>
2022-12-13 10:20:49 -0300

    resolvida a parte 1 - criar um calendário dinamicamente

--- fundamentos/secao-05-js-dom-eventos-web-storage/dia-03-js-eventos/exercicios-agora-pratica/script.js
+++ fundamentos/secao-05-js-dom-eventos-web-storage/dia-03-js-eventos/exercicios-agora-pratica/script.js
@@ -14,4 +14,50 @@ const createDaysOfTheWeek = () => {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+
+// PARTE 1
+//   Crie um calendário dinamicamente.
+// O array decemberDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Sua função deve criar dinamicamente cada dia do calendário e os adicionar dentro da tag <ul>.
+// A tag <ul> deve conter o id 'days';
+// Os dias devem estar contidos em uma tag <li>, e todos devem ter a classe day. Ex: <li class="day">3</li>;
+// Os dias 24, 25 e 31 são feriados e, além da classe day, devem conter também a classe holiday. Ex: <li class="day holiday">24</li>;
+// Os dias 4, 11, 18 e 25 são sextas-feiras. Eles devem conter a classe day e a classe friday. Ex: <li class="day friday">4</li>.
+
+const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+const weekDays = document.querySelector('.week-days');
+weekDays.id = 'days';
+
+for (let index = 0; index < decemberDaysList.length; index += 1) {
   
+  const addWeekDaysLi = document.createElement('li');
+  addWeekDaysLi.innerHTML = `${decemberDaysList[index]}`;
+  addWeekDaysLi.className = 'day';
+  weekDays.appendChild(addWeekDaysLi);
+
+  if (decemberDaysList [index] === 24 || decemberDaysList [index] === 25 || decemberDaysList [index] === 31) {
+    addWeekDaysLi.classList.add('holiday');
+  }
+  
+  if (decemberDaysList [index] === 4 || decemberDaysList [index] === 11 || decemberDaysList [index] === 18 || decemberDaysList [index] === 25) {
+    addWeekDaysLi.classList.add('friday');
+  }
+  // console.log(addWeekDaysLi);
+}
+
+// PARTE 2
+// Implemente uma função que crie dinamicamente um botão com o nome “Feriados”;
+// Sua função deve receber um parâmetro com a string 'Feriados';
+// Adicione a este botão a ID "btn-holiday";
+// Adicione este botão como filho/filha da tag <div> com classe "buttons-container".
+
+
+
+
+
+
+
+
+
+
+
