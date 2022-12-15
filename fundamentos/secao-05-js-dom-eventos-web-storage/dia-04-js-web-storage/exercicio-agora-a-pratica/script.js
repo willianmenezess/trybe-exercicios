95d174ced5e94918e5c2bf7c4c7727976d4c7609
willianmenezess <willmenezes@outlook.com.br>
2022-12-15 13:16:51 -0300

    alinhando as 5 divs de botões lado a lado e cada uma com 20% da largura da página

--- fundamentos/secao-05-js-dom-eventos-web-storage/dia-04-js-web-storage/exercicio-agora-a-pratica/script.js
+++ fundamentos/secao-05-js-dom-eventos-web-storage/dia-04-js-web-storage/exercicio-agora-a-pratica/script.js
@@ -43,29 +43,45 @@ const createDivs = () => {
     for (let i = 1 ; i <= 4 ; i += 1) {
     const createDiv = document.createElement('div');
     createDiv.id = `div-${[i]}`;
+    createDiv.style.width = '20%';
     getDivHeader.appendChild(createDiv);
     }
 }
 
 
-const createButtonsDiv1 = () => {
-    const getDiv1 = document.getElementById('div-1');
-    for (let index = 0; index < 4; index += 1) {
-    const createBtnDiv1 = document.createElement('button');
-    getDiv1.appendChild(createBtnDiv1);
-    console.log(createBtnDiv1);
-    }
-}
 
 const generateDivButtons = () => {
     const createDivHeader = document.createElement('div');
     createDivHeader.id = 'div-all-inputs';
+    createDivHeader.style.display = 'flex';
+    createDivHeader.style.justifyContent = 'center';
+    // createDivHeader.style.gap = '20px';
+    createDivHeader.style.border = '1px solid green';
     const getHeader = document.getElementById('header');
     getHeader.appendChild(createDivHeader);
     createDivs();
     createButtonsDiv1();
+    createButtonsDiv2();
 }
 
+const createButtonsDiv1 = () => {
+    const getDiv1 = document.getElementById('div-1');
+    for (let index = 0; index < 4; index += 1) {
+    const createBtnDiv1 = document.createElement('button');
+    createBtnDiv1.style.display = 'block';
+    getDiv1.appendChild(createBtnDiv1);
+    }
+}
+
+const createButtonsDiv2 = () => {
+    const getDiv2 = document.getElementById('div-2');
+    for (let index = 0; index < 3; index += 1) {
+    const createBtnDiv2 = document.createElement('button');
+    createBtnDiv2.style.display = 'block';
+    getDiv2.appendChild(createBtnDiv2);
+    // console.log(createBtnDiv2);
+    }
+}
 
 
 // EXECUTAR MINHAS FUNÇÕES
