7d49fb7bb5ee9a7136f8397261d2071c8ba44ba2
willianmenezess <willmenezes@outlook.com.br>
2023-01-19 13:34:39 -0300

    resolvida parte 3 - organizando lições

--- fundamentos/secao-07-introducao-a-js6-e-testes-unitarios/dia-01-fluxo-de-excecoes-e-maniip-objetos/teste.js
+++ fundamentos/secao-07-introducao-a-js6-e-testes-unitarios/dia-01-fluxo-de-excecoes-e-maniip-objetos/teste.js
@@ -1,39 +1,13 @@
-const order = {
-    name: 'Rafael Andrade',
-    phoneNumber: '11-98763-1416',
-    address: {
-      street: 'Rua das Flores',
-      number: '389',
-      apartment: '701',
-    },
-    order: {
-      pizza: {
-        marguerita: {
-          amount: 1,
-          price: 25,
-        },
-        pepperoni: {
-          amount: 1,
-          price: 20,
-        },
-      },
-      drinks: {
-        coke: {
-          type: 'Coca-Cola Zero',
-          price: 10,
-          amount: 1,
-        },
-      },
-      delivery: {
-        deliveryPerson: 'Ana Silveira',
-        price: 5,
-      },
-    },
-    payment: {
-      total: 60,
-    },
-  };
 
-  
-  const drinks = order.order.drinks.coke.type;
-  console.log(drinks); 
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const valuesList = (object) => {
+  const valuesList = Object.values(object);
+  console.log(valuesList);
+};
+valuesList(lesson3);
