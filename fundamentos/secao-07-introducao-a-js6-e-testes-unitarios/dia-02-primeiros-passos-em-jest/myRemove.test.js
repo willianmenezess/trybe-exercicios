6115e8087985e96b31cafe7e909d004a559e088c
willianmenezess <willmenezes@outlook.com.br>
2023-01-19 19:46:19 -0300

    resolvido exercicio 1 - teste myRemove

--- fundamentos/secao-07-introducao-a-js6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/myRemove.test.js
+++ fundamentos/secao-07-introducao-a-js6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/myRemove.test.js
@@ -4,12 +4,16 @@ const { myRemove } = require('./myRemove.js');
 
 describe('remove number from array', () => {
 
-    test('remove number 3', () => {
+    test('remove numero 3', () => {
         expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
     });
 
-    test('remove number 3', () => {
-        expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+    test('remove numero 3 e testa se é diferente do array inicial', () => {
+        expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+    });
+
+    test('remove numero 5', () => {
+        expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
     });
 
 
