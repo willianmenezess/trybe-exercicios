a6ae99e70ddf63b48a343df9bd22c1853e7f6beb
willianmenezess <willmenezes@outlook.com.br>
2023-01-28 12:11:52 -0300

    resolvido desafio3.js

---
+++ fundamentos/secao-08-higher-order-functions-do-js-es6/dia-03-hof-filter-e-reduce/desafio4.js
@@ -0,0 +1,14 @@
+const names = [
+    'Aanemarie', 'Adervandes', 'Akifusa',
+    'Abegildo', 'Adicellia', 'Aladonata',
+    'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+const expectedResult = 20;
+
+const countA = () => {
+    // retorne seu código aqui
+}
+
+// 4 - Retorne a quantidade de vezes que a letra a maiúscula ou minúscula aparece no array de nomes.
+// De olho na dica 👀: faça com que o array de nomes seja um array de letras.
