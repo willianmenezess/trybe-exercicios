498fb487d7c31a86d416051b703057b31a27723f
willianmenezess <willmenezes@outlook.com.br>
2023-01-28 00:46:03 -0300

    resolvido exercise2.js

--- fundamentos/secao-08-higher-order-functions-do-js-es6/dia-03-hof-filter-e-reduce/exercise1.js
+++ fundamentos/secao-08-higher-order-functions-do-js-es6/dia-03-hof-filter-e-reduce/exercise1.js
@@ -1,5 +1,6 @@
 const data = require('./database.js');
 
+// 1 - Filtre todos os objetos do gênero ficção científica ou fantasia.
 const fantasyOrScienceFiction = () => {
     // escreva seu código aqui
     return data.books.filter((book) => book.genre === 'Fantasia' || book.genre === 'Ficção Científica');
---
+++ fundamentos/secao-08-higher-order-functions-do-js-es6/dia-03-hof-filter-e-reduce/exercise3.js
@@ -0,0 +1,9 @@
+const data = require('./database.js');
+
+// 3 - Faça uma função que retorne os nomes dos livros, dado o ano de nascimento das pessoas autoras.
+const expectedResult = [ 'Fundação', 'Duna' ];
+
+const booksByAuthorBirthYear = (birthYear) => {
+  // escreva seu código aqui
+}
+const result = booksByAuthorBirthYear(1920);
