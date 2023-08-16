2e59982de3f3350c152a7e62889e23ec8df6b2f0
willianmenezess <willmenezes@outlook.com.br>
2023-08-16 14:41:08 -0300

    feat: resolve exercicio 6

--- back-end/secao-9-POO-e-SOLID/dia-01-introducao-a-POO/exercicio-do-dia/src/Data.ts
+++ back-end/secao-9-POO-e-SOLID/dia-01-introducao-a-POO/exercicio-do-dia/src/Data.ts
@@ -41,6 +41,67 @@ class Data {
     set year(value: number) {
       this._year = value;
     }
+
+    getMonthName(): string {
+        const months = [
+          'janeiro',
+          'fevereiro',
+          'março',
+          'abril',
+          'maio',
+          'junho',
+          'julho',
+          'agosto',
+          'setembro',
+          'outubro',
+          'novembro',
+          'dezembro',
+        ];
+    
+        return months[this.month - 1];
+    }
+
+	isLeapYear(): boolean {
+		return this._year % 4 === 0;
+	}
+
+	compare(date: Data): number {
+		const currentDateStr = `${this.year}-${this.month}-${this.day}`;
+	
+		const dateStr = `${date.year}-${date.month}-${date.day}`;
+	
+		if (new Date(currentDateStr) > new Date(dateStr)) return 1;
+		if (new Date(currentDateStr) < new Date(dateStr)) return -1;
+	
+		return 0;
+	}
+
+	format(formatting: string): string {
+		const conditions: boolean[] = [
+		  (!formatting.match(/a{2,4}/g)), // verifica se possui o ano na formatação
+		  (!formatting.match(/m{2}/g) && !formatting.match(/M{1}/g)), // verifica se tem o mês na formatação
+		  (!formatting.match(/d{2}/g)), // verifica se tem o dia na formatação
+		];
+	
+		if (conditions.every((cond) => cond)) {
+		  throw new Error(`O formato passado é inválido: ${formatting}`);
+		}
+	
+		const day = this.day > 9 ? this.day.toString() : `0${this.day.toString()}`;
+		const month = this.month > 9 
+		  ? this.month.toString() 
+		  : `0${this.month.toString()}`;
+	
+		const year = this.year.toString();
+	
+		const dateFormatting = formatting
+		  .replace('dd', day).replace('mm', month)
+		  .replace('M', this.getMonthName()).replace('aaaa', year)
+		  .replace('aa', year.substr(-2));
+	
+		return dateFormatting;
+	  }
+
   }
   
   // Testando a classe Data
