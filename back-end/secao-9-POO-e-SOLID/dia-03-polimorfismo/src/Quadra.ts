70c731bde655f55fb3bc873fd14f512d8aa9583d
willianmenezess <willmenezes@outlook.com.br>
2023-08-18 10:24:59 -0300

    feat: resolve exercicios 1 a 4

--- back-end/secao-9-POO-e-SOLID/dia-03-polimorfismo/src/Quadra.ts
+++ back-end/secao-9-POO-e-SOLID/dia-03-polimorfismo/src/Quadra.ts
@@ -1,4 +1,8 @@
 // src/Quadra.ts
-class Quadra { }
+import IAgenda from "./interfaces/IAgenda";
+
+abstract class Quadra {
+	protected abstract reservar<T>(horaReserva: Date): IAgenda<T>;
+}
 
 export default Quadra;
