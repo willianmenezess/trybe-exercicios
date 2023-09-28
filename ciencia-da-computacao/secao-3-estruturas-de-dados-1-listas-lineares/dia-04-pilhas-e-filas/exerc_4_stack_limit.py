688e138c1a888edbf78d82f0382fe7f43f55a7fa
willianmenezess <willmenezes@outlook.com.br>
2023-09-28 13:49:32 -0300

    feat: resolve exercicio 4

---
+++ ciencia-da-computacao/secao-3-estruturas-de-dados-1-listas-lineares/dia-04-pilhas-e-filas/exerc_4_stack_limit.py
@@ -0,0 +1,26 @@
+from stack import Stack
+
+
+class StackOverflow(Exception):
+    pass
+
+
+class LimitStack(Stack):
+    def __init__(self, limit):
+        super().__init__()
+        self.limit = limit
+
+    def push(self, value):
+        if self.size() + 1 > self.limit:
+            raise StackOverflow()
+        super().push(value)
+
+
+if __name__ == "__main__":
+    stack = LimitStack(2)
+    stack.push(1)
+    stack.push(-2)
+    try:
+        stack.push(5)
+    except StackOverflow:
+        print('The Stack is full')
