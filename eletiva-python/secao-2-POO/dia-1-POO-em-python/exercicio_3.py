0e49feff73043f4369bafe7e29cdead88ab152ac
willianmenezess <willmenezes@outlook.com.br>
2023-10-26 14:42:57 -0300

    feat: resolve exercicio 3

---
+++ eletiva-python/secao-2-POO/dia-1-POO-em-python/exercicio_3.py
@@ -0,0 +1,13 @@
+class Livro():
+    def __init__(self, titulo: str, autor: str, paginas: int):
+        self.titulo = titulo
+        self.autor = autor
+        self.paginas = paginas
+
+    def description(self) -> str:
+        return f"{self.titulo}, de {self.autor}, tem {self.paginas} páginas."
+
+
+if __name__ == "__main__":
+    book1 = Livro("Cabeça Fria, Coração Quente", "Abel Ferreira", 250)
+    print(book1.description())
